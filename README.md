376a65b881a9e6d78d1c6ef404f90c3d2ac1716a
Roy de Kleijn <dekleijn.roy@gmail.com>
2020-08-13 01:27:13 +0200

    updated readme

--- README.md
+++ README.md
@@ -2,3 +2,102 @@
 [![Maven Central](https://img.shields.io/maven-central/v/io.testsmith/hsac-fitnesse-eyes.svg?maxAge=86400)](https://mvnrepository.com/artifact/io.testsmith/hsac-fitnesse-eyes)
 
 This project brings Applitools Eyes to FitNesse HSAC.
+
+## Available commands
+
+|eyes open_+appname+_ _+testname+_                           |
+|eyes set value _+value+_ for agent id                                                              |
+|eyes set value _+value+_ for app name                                                              |
+|eyes set value _+value+_ for baseline branch name                                                  |
+|eyes set value _+value+_ for baseline env name                                                     |
+|eyes set value _+value+_ for branch name                                                           |
+|eyes set value _+value+_ for debug screenshots path                                                |
+|eyes set value _+value+_ for debug screenshots prefix                                              |
+|eyes set value _+width+_ x _+height+_ for explicit viewport size                    |
+|eyes set value _+boolvalue+_ for force full page screenshot                                            |
+|eyes set value _+boolvalue+_ for hide caret                                                            |
+|eyes set value _+boolvalue+_ for hide scrollbars                                                       |
+|eyes set value _+value+_ for host app                                                              |
+|eyes set value _+value+_ for host os                                                               |
+|eyes set value _+boolvalue+_ for ignore caret                                                          |
+|eyes set value _+boolvalue+_ for is disabled                                                           |
+|eyes set value _+value+_ for match level |content, exact, layout, layout2, none                                                          |
+|eyes set value _+value+_ for match timeout                                                         |
+|eyes set value _+value+_ for parent branch name                                                    |
+|eyes set value _+value+_ for rotation                                                              |
+|eyes set value _+boolvalue+_for save debug screenshots                                                |
+|eyes set value _+boolvalue+_ for save diffs                                                            |
+|eyes set value _+boolvalue+_ for save new tests                                                        |
+|eyes set value _+value+_ for scale ratio                                                           |
+|eyes set value _+boolvalue+_ for scroll to region                                                      |
+|eyes set value _+boolvalue+_ for send dom                                                              |
+|eyes set value _+value+_ for server url                                                            |
+|eyes set value _+value+_ for stitch mode |css, scroll                                                          |
+|eyes set value _+value+_for stitch overlap                                                        |
+|eyes set value _+width+_ x _+height+_ for viewport size                             |
+|eyes set value _+value+_ for wait before screenshots                                               |
+|eyes get agent id                                                                            |
+|eyes get api key                                                                             |
+|eyes get app name                                                                            |
+|eyes get baseline branch name                                                                |
+|eyes get baseline env name                                                                   |
+|eyes get batch                                                                               |
+|eyes get branch name                                                                         |
+|eyes get configuration                                                                       |
+|eyes get debug screenshots path                                                              |
+|eyes get debug screenshots prefix                                                            |
+|eyes get device pixel ratio                                                                  |
+|eyes get force full page screenshot                                                          |
+|eyes get hide caret                                                                          |
+|eyes get hide scrollbars                                                                     |
+|eyes get host app                                                                            |
+|eyes get host os                                                                             |
+|eyes get ignore caret                                                                        |
+|eyes get is disabled                                                                         |
+|eyes get is open                                                                             |
+|eyes get match timeout                                                                       |
+|eyes get parent branch name                                                                  |
+|eyes get proxy                                                                               |
+|eyes get rotation                                                                            |
+|eyes get save debug screenshots                                                              |
+|eyes get save diffs                                                                          |
+|eyes get save new tests                                                                      |
+|eyes get scale ratio                                                                         |
+|eyes get scroll to region                                                                    |
+|eyes get server url                                                                          |
+|eyes get stitch mode                                                                         |
+|eyes get stitch overlap                                                                      |
+|eyes get viewport size                                                                       |
+|eyes get wait before screenshots                                                             |
+|eyes is send dom                                                                             |
+|eyes check element _+place+_                                                        |
+|eyes check element _+place+_ and tag it _+tagname+_|
+|eyes check element _+place+_ and tag it _+tagname+_ with match timeout _+timeout+_|
+|eyes check region _+place+_ and tag it _+tagname+_                              |
+|eyes check region _+place+_                                                                          |
+|eyes check region _+place+_ and do not stitch content                                                 |
+|eyes check region _+place+_ and tag it _+tagname+_                        |
+|eyes check region _+place+_ and tag it _+tagname+_ and do not stitch content                     |
+|eyes check region _+place+_ and tag it _+tagname+_ with match timeout _+timeout+_                    |
+|eyes check region _+place+_ and tag it _+tagname+_ with match timeout _+timeout+_ and do not stitch content|
+|eyes check window                                                                                                                  |
+|eyes check window and tag it _+tagname+_                                                                   |
+|eyes check window fullpage and tag it _+tagname+_                                                                        |
+|eyes check window and tag it _+tagname+_ with match timeout _+timeout+_                                                    |
+|eyes close async                                                                                                                   |
+
+## How to use it?
+
+In the setup you need to set the key, like this:
+
+bc.. |eyes set value                  |!-zir0ZyKljiY00oHjXjfuxzm2eLr1AV5MC6l84BEKY3Q110-!|for api key|
+
+A testscript can look like this:
+
+bc..
+|script                                      |
+|open      |${URL}                           |
+|eyes open;|Hackathon app 0.1|Form components|
+|eyes check window                           |
+|eyes close async                            |
+|eyes get results            |
--- src/main/java/nl/testsmith/fitnesse/fixture/slim/EyesFixture.java
+++ src/main/java/nl/testsmith/fitnesse/fixture/slim/EyesFixture.java
@@ -1,11 +1,16 @@
 package nl.testsmith.fitnesse.fixture.slim;
 
 import com.applitools.eyes.EyesRunner;
+import com.applitools.eyes.MatchLevel;
+import com.applitools.eyes.RectangleSize;
+import com.applitools.eyes.TestResultContainer;
 import com.applitools.eyes.selenium.ClassicRunner;
 import com.applitools.eyes.selenium.Eyes;
+import com.applitools.eyes.selenium.StitchMode;
+import com.applitools.eyes.selenium.positioning.ImageRotation;
 import nl.hsac.fitnesse.fixture.Environment;
 import nl.hsac.fitnesse.fixture.slim.SlimFixture;
-
+import nl.hsac.fitnesse.fixture.slim.SlimFixtureException;
 
 /**
  * Fixture to integrate Applitools Eyes
@@ -27,33 +32,415 @@ public class EyesFixture extends SlimFixture {
 
     }
 
-    public void eyesApiKey(String apiKey) {
-        eyes.setApiKey(apiKey);
-    }
-
     public void eyesOpen(String appName, String testName) {
         eyes.open(Environment.getInstance().getSeleniumHelper().driver(), appName, testName);
     }
 
 
-    public void eyesCheckWindow(String tag) {
+    /**
+     * Check Element
+     */
+    public void eyesCheckElement(String place) {
+        eyes.checkElement(Environment.getInstance().getSeleniumHelper().getElement(place));
+    }
+
+    public void eyesCheckElementAndTagIt(String place, String tag) {
+        eyes.checkElement(Environment.getInstance().getSeleniumHelper().getElement(place), tag);
+    }
+
+    public void eyesCheckElementAndTagItWithMatchTimeout(String place, String tag, int matchTimeout) {
+        eyes.checkElement(Environment.getInstance().getSeleniumHelper().getElement(place), matchTimeout, tag);
+    }
+
+    /**
+     * Check Window
+     */
+    public void eyesCheckWindow() {
+        eyes.checkWindow();
+    }
+
+    public void eyesCheckWindowAndTagIt(String tag) {
         eyes.checkWindow(tag);
+    }
+
+    public void eyesCheckWindowFullpageAndTagIt(String tag) {
+        eyes.checkWindow(tag, true);
+    }
+
+    public void eyesCheckWindowAndTagItWithMatchTimeout(String tag, int matchTimeout) {
+        eyes.checkWindow(matchTimeout, tag);
+    }
+
+    /**
+     * Check Region
+     */
+    public void eyesCheckRegion(String place) {
+        eyes.checkRegion(Environment.getInstance().getSeleniumHelper().getElement(place));
+    }
+
+    public void eyesCheckRegionAndDoNotStitchContent(String place) {
+        eyes.checkRegion(Environment.getInstance().getSeleniumHelper().getElement(place), false);
+    }
+
+    public void eyesCheckRegionAndTagIt(String place, String tag) {
+        eyes.checkRegion(Environment.getInstance().getSeleniumHelper().getElement(place), tag);
+    }
+
+    public void eyesCheckRegionAndTagItAndDoNotStitchContent(String place, String tag) {
+        eyes.checkRegion(Environment.getInstance().getSeleniumHelper().getElement(place), tag, false);
+    }
+
+    public void eyesCheckRegionAndTagItWithMatchTimeout(String place, String tag, int matchTimeout) {
+        eyes.checkRegion(Environment.getInstance().getSeleniumHelper().getElement(place), matchTimeout, tag);
+    }
+
+    public void eyesCheckRegionAndTagItWithMatchTimeoutAndDoNotStitchContent(String place, String tag, int matchTimeout) {
+        eyes.checkRegion(Environment.getInstance().getSeleniumHelper().getElement(place), matchTimeout, tag, false);
+    }
+
+    /**
+     * Getters
+     */
+
+    public String eyesGetAgentId() {
+        return eyes.getAgentId();
+    }
+
+    public String eyesGetApiKey() {
+        return eyes.getApiKey();
+    }
+
+    public String eyesGetAppName() {
+        return eyes.getAppName();
+    }
+
+    public String eyesGetBaselineBranchName() {
+        return eyes.getBaselineBranchName();
+    }
+
+    public String eyesGetBaselineEnvName() {
+        return eyes.getBaselineEnvName();
+    }
+
+    public String eyesGetBatch() {
+        return eyes.getBatch().toString();
+    }
+
+    public String eyesGetBranchName() {
+        return eyes.getBranchName();
+    }
+
+    public String eyesGetConfiguration() {
+        return eyes.getConfiguration().toString();
+    }
+
+    public String eyesGetDebugScreenshotsPath() {
+        return eyes.getDebugScreenshotsPath();
+    }
+
+    public String eyesGetDebugScreenshotsPrefix() {
+        return eyes.getDebugScreenshotsPrefix();
+    }
+
+    public double eyesGetDevicePixelRatio() {
+        return eyes.getDevicePixelRatio();
+    }
+
+    public boolean eyesGetForceFullPageScreenshot() {
+        return eyes.getForceFullPageScreenshot();
+    }
+
+    public boolean eyesGetHideCaret() {
+        return eyes.getHideCaret();
+    }
+
+    public boolean eyesGetHideScrollbars() {
+        return eyes.getHideScrollbars();
+    }
+
+    public String eyesGetHostApp() {
+        return eyes.getHostApp();
+    }
+
+    public String eyesGetHostOs() {
+        return eyes.getHostOS();
+    }
+
+    public boolean eyesGetIgnoreCaret() {
+        return eyes.getIgnoreCaret();
+    }
+
+    public boolean eyesGetIsDisabled() {
+        return eyes.getIsDisabled();
+    }
+
+    public boolean eyesGetIsOpen() {
+        return eyes.getIsOpen();
+    }
+
+    public int eyesGetMatchTimeout() {
+        return eyes.getMatchTimeout();
+    }
+
+    public String eyesGetParentBranchName() {
+        return eyes.getParentBranchName();
+    }
+
+    public String eyesGetProxy() {
+        try {
+            return eyes.getProxy().toString();
+        } catch (NullPointerException e) {
+            throw new SlimFixtureException(false, "Proxy not set.");
+        }
+    }
+
+    public int eyesGetRotation() {
+        try {
+            return eyes.getRotation().getRotation();
+        } catch (NullPointerException e) {
+            throw new SlimFixtureException(false, "Rotation not set.");
+        }
+    }
+
+    public boolean eyesGetSaveDebugScreenshots() {
+        return eyes.getSaveDebugScreenshots();
+    }
+
+    public boolean eyesGetSaveDiffs() {
+        return eyes.getSaveDiffs();
+    }
+
+    public boolean eyesGetSaveNewTests() {
+        return eyes.getSaveNewTests();
+    }
+
+    public double eyesGetScaleRatio() {
+        return eyes.getScaleRatio();
+    }
+
+    public boolean eyesGetScrollToRegion() {
+        return eyes.getScrollToRegion();
+    }
+
+    public String eyesGetServerUrl() {
+        return eyes.getServerUrl().toString();
+    }
+
+    public String eyesGetStitchMode() {
+        return eyes.getStitchMode().toString();
+    }
+
+    public int eyesGetStitchOverlap() {
+        return eyes.getStitchOverlap();
+    }
+
+    public String eyesGetViewportSize() {
+        return eyes.getViewportSize().toString();
+    }
+
+    public int eyesGetWaitBeforeScreenshots() {
+        return eyes.getWaitBeforeScreenshots();
+    }
+
+    public boolean eyesIsSendDom() {
+        return eyes.isSendDom();
+    }
+
+    /**
+     * Setters
+     */
+    public void eyesSetValueForAgentId(String agentId) {
+        eyes.setAgentId(agentId);
+    }
+
+    public void eyesSetValueForApiKey(String apiKey) {
+        eyes.setApiKey(apiKey);
+    }
+
+    public void eyesSetValueForAppName(String appName) {
+        eyes.setAppName(appName);
+    }
+
+    public void eyesSetValueForBaselineBranchName(String baselineBranchName) {
+        eyes.setBranchName(baselineBranchName);
+    }
+
+    public void eyesSetValueForBaselineEnvName(String baselineEnvName) {
+        eyes.setBaselineEnvName(baselineEnvName);
+    }
+
+    //    setBatch()
+    public void eyesSetValueForBranchName(String branchName) {
+        eyes.setBranchName(branchName);
+    }
+
+    //    setConfiguration()
+    public void eyesSetValueForDebugScreenshotsPath(String pathToSave) {
+        eyes.setDebugScreenshotsPath(pathToSave);
+    }
+
+    public void eyesSetValueForDebugScreenshotsPrefix(String prefix) {
+        eyes.setDebugScreenshotsPrefix(prefix);
+    }
+
+    public void eyesSetValueXForExplicitViewportSize(int width, int height) {
+        eyes.setExplicitViewportSize(new RectangleSize(width, height));
+    }
+
+    public void eyesSetValueForForceFullPageScreenshot(boolean shouldForce) {
+        eyes.setForceFullPageScreenshot(shouldForce);
+    }
+
+    public void eyesSetValueForHideCaret(boolean hideCaret) {
+        eyes.setHideCaret(hideCaret);
+    }
+
+    public void eyesSetValueForHideScrollbars(boolean shouldHide) {
+        eyes.setHideScrollbars(shouldHide);
+
+    }
+
+    public void eyesSetValueForHostApp(String hostApp) {
+        eyes.setHostApp(hostApp);
+
+    }
+
+    public void eyesSetValueForHostOs(String hostOs) {
+        eyes.setHostOS(hostOs);
+
+    }
+
+    public void eyesSetValueForIgnoreCaret(boolean ignoreCaret) {
+        eyes.setIgnoreCaret(ignoreCaret);
+    }
+
+    public void eyesSetValueForIsDisabled(boolean isDisplayed) {
+        eyes.setIsDisabled(isDisplayed);
+    }
+
+    public void eyesSetValueForMatchLevel(String level) {
+        switch (level.toLowerCase()) {
+            case "content":
+                eyes.setMatchLevel(MatchLevel.CONTENT);
+                break;
+            case "exact":
+                eyes.setMatchLevel(MatchLevel.EXACT);
+                break;
+            case "layout":
+                eyes.setMatchLevel(MatchLevel.LAYOUT);
+                break;
+            case "layout2":
+                eyes.setMatchLevel(MatchLevel.LAYOUT2);
+                break;
+            case "none":
+                eyes.setMatchLevel(MatchLevel.NONE);
+                break;
+            default:
+                throw new SlimFixtureException(false, "Match level unknown.");
+        }
+    }
+
+
+    public void eyesSetValueForMatchTimeout(int ms) {
+        eyes.setMatchTimeout(ms);
+    }
+
+    public void eyesSetValueForParentBranchName(String branchName) {
+        eyes.setParentBranchName(branchName);
+
+    }
+//    setProxy()
+
+    public void eyesSetValueForRotation(int rotation) {
+        eyes.setRotation(new ImageRotation(rotation));
+    }
+
+    public void eyesSetValueForSaveDebugScreenshots(boolean saveDebugScreenshots) {
+        eyes.setSaveDebugScreenshots(saveDebugScreenshots);
+    }
+
+    public void eyesSetValueForSaveDiffs(boolean saveDiffs) {
+        eyes.setSaveDiffs(saveDiffs);
+    }
+
+    public void eyesSetValueForSaveNewTests(boolean saveNewTests) {
+        eyes.setSaveNewTests(saveNewTests);
+    }
+
+    public void eyesSetValueForScaleRatio(double scaleRation) {
+        eyes.setScaleRatio(scaleRation);
+    }
+
+    public void eyesSetValueForScrollToRegion(boolean shouldScroll) {
+        eyes.setScrollToRegion(shouldScroll);
+    }
+
+    public void eyesSetValueForSendDom(boolean isSendDom) {
+        eyes.setSendDom(isSendDom);
+    }
+
+    public void eyesSetValueForServerUrl(String serverUrl) {
+        eyes.setServerUrl(serverUrl);
+    }
+
+    public void eyesSetValueForStitchMode(String stitchMode) {
+        switch (stitchMode.toLowerCase()) {
+            case "css":
+                eyes.setStitchMode(StitchMode.CSS);
+                break;
+            case "scroll":
+                eyes.setStitchMode(StitchMode.SCROLL);
+                break;
+            default:
+                throw new SlimFixtureException(false, "Stitch mode unknown.");
+        }
+    }
+
+    public void eyesSetValueForStitchOverlap(int pixels) {
+        eyes.setStitchOverlap(pixels);
+    }
+
+    public void eyesSetValueXForViewportSize(int width, int height) {
+        try {
+            eyes.setViewportSize(Environment.getInstance().getSeleniumHelper().driver(), new RectangleSize(width, height));
+        } catch (Exception e) {
+            throw new SlimFixtureException(false, e.getMessage());
+        }
+    }
+
+    public void eyesSetValueForWaitBeforeScreenshots(int waitBeforeScreenshots) {
+        eyes.setWaitBeforeScreenshots(waitBeforeScreenshots);
     }
 
     public void eyesClose() {
         eyes.close();
     }
 
+    public void eyesCloseWithException() {
+        eyes.close(true);
+    }
+
     public void eyesCloseAsync() {
         eyes.closeAsync();
+    }
+
+    public void eyesAbort() {
+        eyes.abort();
+    }
+
+    public void eyesAbortAsync() {
+        eyes.abortAsync();
     }
 
     public void eyesAbortIfNotClosed() {
         eyes.abortIfNotClosed();
     }
 
-    public String eyesgetResults() {
-        return runner.getAllTestResults().toString();
+    public String eyesGetResults() {
+        for (TestResultContainer result : runner.getAllTestResults()) {
+            System.out.println(result.toString());
+        }
+        return "";
     }
 
 }
